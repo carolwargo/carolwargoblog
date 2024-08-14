6325d9b030905d7c5786348b03a71aaa0cd5f69b
Carol Wargo <Carolwargo@gmail.com>
2024-08-14 16:38:55 -0400

    adding User logic

--- api/index.js
+++ api/index.js
@@ -1,20 +1,33 @@
-const express= require('express');
-const cors  = require('cors');
-const app   =express();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose'); // Mongoose import added
+const User = require('./models/User'); // User model import added
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://carolwargo:8DyaILn1LnL4Ttql@cluster0.llebq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect(
+  'mongodb+srv://carolwargo:8DyaILn1LnL4Ttql@cluster0.llebq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+  }
+)
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Could not connect to MongoDB...', err));
 
-
-app.post('/register',(req,res) => {
-    const {username,password} = req.body;
-res.json({requestData:{username,password}});
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.create({username, password});
+  res.json(userDoc);
 });
 
 
-app.listen(4000)
+app.listen(4000, () => {
+  console.log('Server is running on port 4000');
+});
+
 
 
 /*MongoDB LOGIN INFO------->>>>>> 
