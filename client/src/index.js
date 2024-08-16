b5e59220fb9555a5f155e6630107137986fd9191
Carol Wargo <Carolwargo@gmail.com>
2024-08-16 09:19:21 -0400

    uninstall type (quill)

--- client/src/index.js
+++ client/src/index.js
@@ -6,24 +6,12 @@ import reportWebVitals from './reportWebVitals';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-// Customize console.warn to filter out specific warnings
-console.warn = (function(warn) {
-  return function(message, ...args) {
-    if (!message.includes('Listener added for a \'DOMNodeInserted\' mutation event')) {
-      warn.apply(console, [message, ...args]);
-    }
-  };
-})(console.warn);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// ✅ With this
+root.render(<App />);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,95 +1,35 @@
-import React, {useState} from 'react';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
-
-const modules = {
-    toolbar: [
-        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],       // dropdown with defaults from theme
-        ['bold', 'italic', 'underline', 'strike', 'blockquote'],     
-        [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'list': 'check' }],    
-        [{ 'indent': '-1'}, { 'indent': '+1' }],                               
-        ['clean']                       
-    ]
-};
-
-
-const formats = [
-    'header', 
-    'bold', 'italic', 'underline', 'strike', 'blockquote',
-    'list', 'bullet', 'indent',
-    'link', 'image', 
-
-];
 
 export default function CreatePost() {
-const [title, setTitle] = useState('');     
-const [summary, setSummary] = useState('');
-const [content, setContent] = useState('');
-const [files, setFiles] = useState('');
-
-
-async function createNewPost(ev) {
-    const data = new FormData();
-    data.set('title', title);
-    data.set('summary', summary);
-    data.set('content', content);
-    console.log(files);
-    data.set('file', files[0]);
-ev.preventDefault();
-
-await fetch('http://localhost:4000/posts', {
-        method: 'POST',
-        body: data,  
-        credentials: 'include',
-        headers: {
-            'Accept': 'application/json',
-        }
-
-    });
-console.log('Post created');
-}
 
     return (
-
         <div>
-            <div className='w3-container w3-content'>
-                <h1>Create new post here</h1>
-                <form onSubmit={createNewPost}>
-                    <input 
-                    type="text" 
-                    placeholder={'Title'}
-                    value={title}
-                    onChange={(ev) => setTitle(ev.target.value)}
-                    />
-                    <input 
-                    type="text" 
-                    placeholder={'Summary'}
-                    value={summary}
-                    onChange={(ev) => setSummary(ev.target.value)}
-                    />
-                      <input 
-                    type="file" 
-                    onChange={(ev) => setFiles(ev.target.files[0])} // Corrected file handling
-                 
-                    />
-           
-                <ReactQuill
-                
-                value={content} 
-                modules={modules}
-                formats={formats}
-                onChange={newValue => setContent(newValue)}
+            <div className="w3-container w3-content">
+            <h1>Create Post</h1>
+            <form>
+                <input 
+                type="title"
+                placeholder={'title'}
+               />
+                <input 
+                type="summary"
+                placeholder={'summary'}
                 />
-                <button 
-                className='mt-2'>
-                    Create Post
-                    </button>
-                </form>
-                
-                </div>
-        
-        
+                <input 
+                type="context"
+                placeholder={'context'}
+               />
+               <input type="file" 
+               />
+               <textarea 
+               className="w-100 my-1"
+               name="" id=""
+               cols={30} rows={10}
+               >
+               </textarea>
+               <button className="w-100 mb-5 bg-primary">Add Post</button>
+            </form>
         </div>
+        </div>
+    )}
 
-    );
-}
+    
